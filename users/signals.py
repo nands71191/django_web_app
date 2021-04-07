2362389597727b101a767173e0d7d2f69bb15adf
nands71191 <nands71191@gmail.com>
2021-04-08 01:39:32 +0530

    django login

---
+++ users/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.singals import post_save
+from django.contrib.auth.models import user
+from django.dispatch impost receiver
+from .models import Profile
+
+''
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+	instance.profile.save()
+'''
+@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+def save_profile(sender, instance, created, **kwargs):
+    user = instance
+    if created:
+        profile = UserProfile(user=user)
+        profile.save()
+'''
